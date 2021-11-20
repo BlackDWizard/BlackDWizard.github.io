5a655353f0582d72a2716126f893a6c803381d8d
SntBladeDevil <jonathan520419@gmail.com>
2021-11-20 18:37:38 +0800

    0.0.0.31

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { $ } from "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+import * as jQuery from "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
 
 $('.message a').click(function () {
     $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");

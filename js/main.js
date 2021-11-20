9a7d28d4a80a7e0b4a32ef2dffeeade85f1fc7b1
SntBladeDevil <jonathan520419@gmail.com>
2021-11-20 09:23:44 +0800

    0.0.0.17

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, collection, getDocs, getDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDhdF6xcIZKROi23uJjuKzht3sReSKD0K0",
+    authDomain: "checkinsystem-6df61.firebaseapp.com",
+    databaseURL: "https://checkinsystem-6df61-default-rtdb.firebaseio.com",
+    projectId: "checkinsystem-6df61",
+    storageBucket: "checkinsystem-6df61.appspot.com",
+    messagingSenderId: "991651325747",
+    appId: "1:991651325747:web:a5efe0896d8c27baf68046",
+    measurementId: "G-S59YBM9G38"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+auth.onAuthStateChanged(user => {
+
+});
+
+onAuthStateChanged(auth, user => {
+    if (user != null) {
+        console.log("logged in !");
+    } else {
+        console.log("No User");
+    }
+});
+
 function alert1() {
     alert(123);
 }
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+// const path = require('path');
+
+// module.exports = {
+//     mode: 'development',
+//     devtool: 'eval-source-map',
+//     entry: './js/main.js',
+//     output: {
+//         path: path.resolve(__dirname)
+//     }
+// };
